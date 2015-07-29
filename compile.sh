e86ffdd095e0a9c8722b65562c27a84876f427cf
Robbie1977 <robbie.court@gmail.com>
2015-07-29 22:23:25 +0100

    first complete new build

--- compile.sh
+++
@@ -1,2 +0,0 @@
-python ../IndexStackConv/nrrd2wlzDom.py individualDomainFiles/domain template/background.wlz /disk/data/VFBTools/Woolz2013Full/bin/ /disk/data/VFBTools/Fiji/ImageJ-linux64 0.46 0.4612588
-
